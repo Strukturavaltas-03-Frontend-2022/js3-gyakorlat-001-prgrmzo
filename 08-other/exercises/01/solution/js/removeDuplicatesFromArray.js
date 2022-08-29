bc36a1f6566d9dff7d8926252fb2b98072b22087
Zo <pahyzoard@outlook.hu>
2022-08-29 19:59:47 +0200

    mission completed

--- 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
+++ 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
@@ -1,1 +1,9 @@
+// 1. Írj egy függvény kifejezést `removeDuplicatesFromArray` néven, ami a paraméterként megadott tömbből eltávolítja a duplikált elemeket! A tömb csak primítív értékeket tartalmaz!
+// A függvény visszatérési értéke a duplikált elemektől mentes új tömb!
+
+const removeDuplicatesFromArray = (array) => {
+  return Array.from(new Set(array));                // return [...new Set(array)]
+};
+
+
 export default removeDuplicatesFromArray;
--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,43 @@
+// 3. Készíts egy `objectConverter` nevű objektumot, ami az alábbi metódusokkal rendelkezik:
+//    - `arrayToMap(array)`
+//    - `arrayToSet(array)`
+//    - `setToMap(set)`
+//    - `setToArray(set)`
+//    - `mapToArray(map)`
+//    - `mapToSet(map)`
+
+// A fenti metódusok értelemszerűen nem másra szolgálnak, mint az egyik összetett adattípusból konvertálják át
+//  az adatokat egy másikba.
+// Mindegyik metódus visszatérési értéke egy új Array, Map, vagy Set object.
+const objectConverter = {
+  arrayToMap(array) {
+    const arrMap = new Map([]);
+    array.forEach((value, i) => arrMap.set(i, value));
+    return arrMap;
+  },
+  arrayToSet(array) {
+    return new Set(array);
+  },
+  setToMap(set) {
+    const setMap = new Map([]);
+    Array.from(set).forEach((value, i) => setMap.set(i, value));
+    return setMap;
+  },
+  setToArray(set) {
+    return [...set]; // return Array.from(set)
+  },
+  mapToArray(map) {
+    const mapArr = [];
+    map.forEach((value) => {
+      mapArr.push(value);
+    });
+    return mapArr;
+  },
+  mapToSet(map) {
+    const mapSet = [];
+    map.forEach((value) => mapSet.push(value));
+    return new Set(mapSet);
+  },
+};
+
 export default objectConverter;
